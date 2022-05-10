25a3f74258a14e8d5399c26b9d4b55a04cdd5324
Joe Park-Kennaby <joeparkkennaby@gmail.com>
2022-05-10 16:10:06 +0100

    setup cards http service

---
+++ src/services/cards.ts
@@ -0,0 +1,33 @@
+const BASE_URL = "http://deckofcardsapi.com";
+const ROUTE = "/api/deck/new/draw/";
+// http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1
+// http://deckofcardsapi.com/api/deck/new/draw/?count=3
+
+const getEndpoint = () => {
+  return `${BASE_URL}/${ROUTE}`;
+};
+
+const draw = async (count: number) => {
+  const endpoint = getEndpoint();
+
+  const params = new URLSearchParams({
+    count: count.toString(),
+  });
+
+  try {
+    const response = await fetch(`${endpoint}?` + params.toString());
+
+    const updatedResponse = {
+      ...response,
+      data: await response.json(),
+    };
+
+    return updatedResponse;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export default {
+  draw,
+};
